4398b516bd4292ae187a04449da12cb8c43a19f9
rellurua <relluru@amazon.com>
2023-07-12 17:40:26 -0500

    Add files via upload

---
+++ FSharpHelloWorld/Program.fs
@@ -0,0 +1,7 @@
+// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv = 
+  printfn "Hello World"
+  0 // return an integer exit code
